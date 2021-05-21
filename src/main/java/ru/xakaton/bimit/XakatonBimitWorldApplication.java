f7204cbccf27a610f9891fc64efb670b3a542141
Maks <max@bimit.ru>
2021-05-21 19:56:34 +0400

    start

---
+++ src/main/java/ru/xakaton/bimit/XakatonBimitWorldApplication.java
@@ -0,0 +1,13 @@
+package ru.xakaton.bimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XakatonBimitWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XakatonBimitWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/xakaton/bimit/XakatonBimitWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.xakaton.bimit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XakatonBimitWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
