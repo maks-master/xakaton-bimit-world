363b5180adf15c3d24a7bd2474ff5aae67ee57db
Master Maks <master@mifors.com>
2021-05-22 09:53:35 +0400

    Болванка старт

--- src/main/java/ru/xakaton/bimit/XakatonBimitWorldApplication.java
+++ src/main/java/ru/xakaton/bimit/XakatonBimitWorldApplication.java
@@ -2,12 +2,20 @@ package ru.xakaton.bimit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class XakatonBimitWorldApplication {
+public class XakatonBimitWorldApplication extends SpringBootServletInitializer {
 
+	// for war
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(XakatonBimitWorldApplication.class);
+	}
+
+	// for jar
 	public static void main(String[] args) {
 		SpringApplication.run(XakatonBimitWorldApplication.class, args);
 	}
-
 }
